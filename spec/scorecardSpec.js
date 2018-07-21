b340f483214d34421a43b58e1a8a943d4c00bf03
Daniel Lau <daniellau@Daniels-MBP-2.home>
2018-07-21 16:02:51 +0100

    can calculate a spare

--- spec/scorecardSpec.js
+++ spec/scorecardSpec.js
@@ -8,17 +8,30 @@ describe('Scorecard', function() {
 
 
   it("allows a gutter game", function(){
-    for (var i = 0; i < 20; i++){
-      scorecard.roll(0);
-    }
+    rollMultipleTimes(20,0);
     expect(scorecard.score()).toEqual(0);
   });
 
   it("calculates the score", function(){
     scorecard.roll(1);
     scorecard.roll(1);
+    rollMultipleTimes(18,0);
     expect(scorecard.score()).toEqual(2);
   });
 
+  it("calculates the spare", function(){
+    scorecard.roll(5);
+    scorecard.roll(5);
+    scorecard.roll(2);
+    rollMultipleTimes(17,0);
+    expect(scorecard.score()).toEqual(14);
+  });
+
+
+  function rollMultipleTimes(rolls, pins){
+    for(var i = 0; i < rolls; i++){
+      scorecard.roll(pins);
+    }
+  }
 
 });
--- src/scorecard.js
+++ src/scorecard.js
@@ -1,12 +1,38 @@
 function Scorecard(){
-this._score = 0;
+  this._BONUS = 10;
+  this._rolls = [];
 }
 
 
 Scorecard.prototype.roll = function(pins){
-  this._score += pins;
+  this._rolls.push(pins);
 };
 
 Scorecard.prototype.score = function(){
-   return this._score;
+  var rollIndex = 0;
+  var score = 0;
+
+  for(var frame = 0; frame < 10; frame++){
+    if(this._isSpare(rollIndex)){
+      score += this._spareBonus(rollIndex);
+      rollIndex += 2;
+    } else {
+      score += this._normalScore(rollIndex);
+      rollIndex += 2;
+    }
+  }
+  return score;
+};
+
+
+Scorecard.prototype._isSpare = function(rollIndex){
+  return this._rolls[rollIndex] + this._rolls[rollIndex + 1] === 10;
+};
+
+Scorecard.prototype._spareBonus = function(rollIndex){
+  return this._BONUS + this._rolls[rollIndex + 2];
+};
+
+Scorecard.prototype._normalScore = function(rollIndex){
+  return this._rolls[rollIndex] + this._rolls[rollIndex + 1];
 };
